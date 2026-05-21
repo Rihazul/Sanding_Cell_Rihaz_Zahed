145f7f8e606bcf64185e42ba1912784e6ef14898
Rihazul <rihazulislam@gmail.com>
2026-05-21 11:09:23 -0400

    offset error on edge_coverage. 1.3

--- Sanding_Cell_Code/model1cycle/mod1zigzag.py
+++ Sanding_Cell_Code/model1cycle/mod1zigzag.py
@@ -135,10 +135,10 @@ def _generate_zigzag_edge_path(
     raw_y_min = min(y_coords)
     raw_y_max = max(y_coords)
 
-    x_min_edge = raw_x_min + tool3x + edge_offset
-    x_max_edge = raw_x_max - tool3x - edge_offset
-    y_min_edge = raw_y_min + tool3y + edge_offset
-    y_max_edge = raw_y_max - tool3y - edge_offset
+    x_min_edge = raw_x_min + tool3x + 8
+    x_max_edge = raw_x_max - tool3x - 8
+    y_min_edge = raw_y_min + tool3y + 8
+    y_max_edge = raw_y_max - tool3y - 15
 
     edge_points = []
     if edge_coverage:
