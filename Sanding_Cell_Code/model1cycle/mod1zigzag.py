6c2875d208e7523cb52040f8df9f6e2dec8d6bee
Rihazul <rihazulislam@gmail.com>
2026-05-21 11:05:39 -0400

    offset error on edge_coverage. 1.2

--- Sanding_Cell_Code/model1cycle/mod1zigzag.py
+++ Sanding_Cell_Code/model1cycle/mod1zigzag.py
@@ -482,7 +482,7 @@ def testmodel3zigzagsmallfunction(force,innerSandingOffset,cps,movement="both",t
     edge_pathp1, zigzag_pathp1, prepointp1 = _generate_zigzag_edge_path(
         x_coords=x_coords1, y_coords=y_coords1, z_coords=z_coords1,
         innerOffset=corner_offset, innerOffsetX=corner_offset, innerSandingOffset=innerSandingOffset,
-        orientation=zigzag_orientation, edge_coverage=True, edge_offset=6,
+        orientation=zigzag_orientation, edge_coverage=True, edge_offset=10,
         tool_offset_x=active_tool_offset, tool_offset_y=active_tool_offset
     )
     print("edge_pathp=", edge_pathp1)
@@ -904,7 +904,7 @@ def testmodel2zigzagsmallfunction(force,innerSandingOffset,cps,movement="both",t
     edge_pathp1, zigzag_pathp1, prepointp1 = _generate_zigzag_edge_path(
         x_coords=x_coords1, y_coords=y_coords1, z_coords=z_coords1,
         innerOffset=corner_offset, innerOffsetX=corner_offset, innerSandingOffset=innerSandingOffset,
-        orientation=zigzag_orientation, edge_coverage=True, edge_offset=6,
+        orientation=zigzag_orientation, edge_coverage=True, edge_offset=10,
         tool_offset_x=active_tool_offset, tool_offset_y=active_tool_offset
     )
     print("edge_pathp=", edge_pathp1)
@@ -1327,7 +1327,7 @@ def testmodel1zigzagsmallfunction(force,innerSandingOffset,cps,movement="both",t
     edge_pathp1, zigzag_pathp1, prepointp1 = _generate_zigzag_edge_path(
         x_coords=x_coords1, y_coords=y_coords1, z_coords=z_coords1,
         innerOffset=corner_offset, innerOffsetX=corner_offset, innerSandingOffset=innerSandingOffset,
-        orientation=zigzag_orientation, edge_coverage=True, edge_offset=6,
+        orientation=zigzag_orientation, edge_coverage=True, edge_offset=10,
         tool_offset_x=active_tool_offset, tool_offset_y=active_tool_offset
     )
     print("edge_pathp=", edge_pathp1)
